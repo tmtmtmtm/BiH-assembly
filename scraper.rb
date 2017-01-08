c95a1e3c4e737d8db297eb870840ee643a96a169
Tony Bowden <tony@tmtm.com>
2017-01-08 18:15:47 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -11,7 +12,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -30,24 +31,24 @@ def scrape_person(name, url)
   noko = noko_for(url)
 
   izborna = noko.xpath('//td[contains(.,"Izborna")]/following-sibling::td').first.text.tidy
-  unit, entity = izborna.sub('Izborna jedinica ','').split(/\//, 2).map(&:tidy)
+  unit, entity = izborna.sub('Izborna jedinica ', '').split(/\//, 2).map(&:tidy)
 
-  data = { 
-    id: url.to_s[/\?id=(\d+)/, 1],
-    name: noko.css('h1.poslanikNaziv').text.tidy,
+  data = {
+    id:        url.to_s[/\?id=(\d+)/, 1],
+    name:      noko.css('h1.poslanikNaziv').text.tidy,
     sort_name: name,
-    image: noko.css('dt.slika img/@src').text,
-    party: noko.xpath('//td[contains(.,"Stranka")]/following-sibling::td').first.text.tidy,
-    faction: noko.xpath('//td[contains(.,"Klub")]/following-sibling::td').first.text.sub('Klub poslanika ','').tidy,
-    email: noko.xpath('//td[contains(.,"E-mail")]/following-sibling::td').first.text.tidy,
-    area: "#{unit} #{entity}",
-    area_id: "ocd-division/country:ba/unit:#{unit.downcase}/entity:#{entity}",
-    term: 7,
-    source: url.to_s,
+    image:     noko.css('dt.slika img/@src').text,
+    party:     noko.xpath('//td[contains(.,"Stranka")]/following-sibling::td').first.text.tidy,
+    faction:   noko.xpath('//td[contains(.,"Klub")]/following-sibling::td').first.text.sub('Klub poslanika ', '').tidy,
+    email:     noko.xpath('//td[contains(.,"E-mail")]/following-sibling::td').first.text.tidy,
+    area:      "#{unit} #{entity}",
+    area_id:   "ocd-division/country:ba/unit:#{unit.downcase}/entity:#{entity}",
+    term:      7,
+    source:    url.to_s,
   }
   data[:image] = URI.join(url, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
   puts data[:name]
-  ScraperWiki.save_sqlite([:id, :term], data)
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
 
 scrape_list('https://www.parlament.ba/sadrzaj/poslanici/p/Archive.aspx?m=3')
